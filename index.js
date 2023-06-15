31fd009224c5f5f0d076b3a7be62b20731f0fd97
Henry Ngata <henrygathuru2017@gmail.com>
2023-06-15 15:34:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,26 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//
+app.post('/api/shorturl', function(req, res, next) {
+  const url = new URL(req.body.url);
+  const hostname = url.hostname;
+
+  dns.lookup(hostname, (err, address) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ error: 'Invalid URL' });
+    } else {
+      next();
+    }
+  });
+}, function(req, res) {
+  const min = 1;
+  const max = 100000000;
+  const randomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
+  res.json({ original_url: req.body.url, short_url: randomInteger });
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
